bfcbe8ea5f2d8559cb980a0919e34a00dc10b9d4
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2015-05-17 00:52:46 +0530

    Moving News class to its own file
    This would enable it to be used by other scripts as well. Also, moved the unique
    key insertion method inside the News class. Passing only the news entry would be
    enough to add to the database. This maked feeds.py a lot cleaner.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

---
+++ news_db.py
@@ -0,0 +1,36 @@
+import hashlib
+import logging
+from time import mktime
+from datetime import datetime
+from google.appengine.ext import ndb
+
+class News(ndb.Model):
+	"""Class to store news items in GAE NDB"""
+	title = ndb.StringProperty(required=True)
+	date = ndb.DateTimeProperty(required=True)
+	create_date = ndb.DateTimeProperty(auto_now=True)
+	link = ndb.StringProperty(required=True)
+	author = ndb.StringProperty()
+	text = ndb.TextProperty()
+
+	def add_db_unique(self, entry):
+		ndate = entry['datetime']
+		title = entry['title']
+		ptime = entry['print_time']
+		link = entry['link']
+		author = entry['author']
+
+		ky = hashlib.md5(title + ptime).hexdigest()
+		new_key = ndb.Key(News, ky)
+		entry = new_key.get()
+		if entry is None:
+			logging.debug("New news entry found. Adding it to db")
+			dt = datetime.fromtimestamp(mktime(ndate))
+			news = News(title=title, date=dt, link=link,
+					author=author)
+			news.key = new_key
+			news.put()
+			return True
+		else:
+			logging.debug("News entry already present. Skipping.")
+			return False
