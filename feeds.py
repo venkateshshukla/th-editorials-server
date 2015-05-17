44e000ed2c092f59eb61d038e4eefdfb1963978d
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2015-05-17 09:20:46 +0530

    Try Except in case of Connection Error
    Better error handling.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

--- feeds.py
+++ feeds.py
@@ -1,3 +1,4 @@
+import logging
 from news_db import News
 from src.news_feed import get_news_feed
 from src.news_text import get_news_text
@@ -6,10 +7,19 @@ from webapp2 import RequestHandler, WSGIApplication
 class Feeds(RequestHandler):
 	def get(self):
 		self.response.headers['Content-Type'] = 'text/plain'
-		news_list = get_news_feed()
+		try:
+			news_list = get_news_feed()
+		except ConnectionError:
+			logging.error("Error connecting to the RSS feed.")
+			self.response.write("Error connecting to RSS feed.");
+			return
 		num_stored = 0
 		for n in news_list:
-			n['text'] = get_news_text(n['link'])
+			try:
+				n['text'] = get_news_text(n['link'])
+			except ConnectionError:
+				logging.error("Error fetching news link")
+				continue
 			news = News()
 			if news.add_db_unique(n):
 				num_stored += 1
