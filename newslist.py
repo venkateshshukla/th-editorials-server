3d4fca130431da52d9d35713fe01945fa4dd9ca3
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2015-05-25 01:50:15 +0530

    View all the editorials saved in the datastore
    Visit /list to see datewise sorted list of all the editorials of the database.
    This is useful in debugging of the application.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

---
+++ newslist.py
@@ -0,0 +1,27 @@
+import cgi
+import logging
+from webapp2 import RequestHandler, WSGIApplication
+from news_db import News
+
+class NewsList(RequestHandler):
+	def get(self):
+		qry = News.query().order(-News.date)
+		self.response.write('<html><body>')
+		self.response.write('<h3>List of editorials</h3>')
+		i = 0
+		for q in qry:
+			i += 1
+			date = cgi.escape("{:%d-%m-%Y %H:%M:%S}".format(q.date))
+			title = cgi.escape(q.title)
+			author = cgi.escape(q.author)
+			self.response.out.write('<div><p>')
+			self.response.out.write('{}.\t[{}] '.format(i, date))
+			self.response.out.write('<a href={}>{}</a>'.format(q.link, title))
+			if author:
+				self.response.out.write(' - {}'.format(author))
+			self.response.out.write('</p></div>')
+		self.response.write('</body></html>')
+	def post(self):
+		pass
+
+app = WSGIApplication([ ('/list', NewsList), ], debug=True)
