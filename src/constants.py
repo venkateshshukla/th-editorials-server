80c0fc699989a4dbd27ddd2eae9b2572a58bfa1d
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2015-05-26 01:44:52 +0530

    A sample url of TH for tests
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

--- src/constants.py
+++ src/constants.py
@@ -3,4 +3,5 @@ class AppUrl:
 	BASE = "http://www.thehindu.com/"
 	OPINION = BASE + "opinion/?service=rss"
 	EDITORIAL = BASE + "opinion/editorial/?service=rss"
+	SAMPLE = BASE +	"opinion/op-ed/a-super-visit-in-the-season-of-hope/article7214799.ece"
 	RSS_ARGS = "?utm_source=RSS_Feed&utm_medium=RSS&utm_campaign=RSS_Syndication"
