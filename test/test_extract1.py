052d1c410a160466eb8e4a0e3ad681dd3d62cdd6
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2015-06-09 23:23:02 +0530

    Tests modified, added
    Some additional naive tests are added. Cannot dedicate much time yet.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

--- test/test_extract1.py
+++ test/test_extract1.py
@@ -11,18 +11,16 @@ from src.errors import InputError
 
 class CleanCopyTest(unittest.TestCase):
 
-	def test_empty_inp(self):
+	def test_none_inp(self):
 		inp = None
 		out = BeautifulSoup('')
-		soup = BeautifulSoup('<html><body></body></html>')
+		exp = BeautifulSoup('')
 
-		exp = out
-
-		clean_copy(inp, out, soup)
+		clean_copy(inp, out, out)
 
 		self.assertEqual(out, exp, msg="Output changes on None input.")
 
-	def test_empty_out(self):
+	def test_none_out(self):
 		inp = BeautifulSoup('')
 		out = None
 		soup = BeautifulSoup('<html><body></body></html>')
@@ -31,12 +29,54 @@ class CleanCopyTest(unittest.TestCase):
 
 		self.assertIsNone(out, msg="Value is assigned to None output.")
 
-	def test_empty_soup(self):
+	def test_none_soup(self):
 		inp = BeautifulSoup('<html><body></body></html>')
 		out = BeautifulSoup('')
 		soup = None
 
 		self.assertRaises(InputError, clean_copy, inp, out, soup)
+
+	def test_empty_inp(self):
+		inp = BeautifulSoup('')
+		out = BeautifulSoup('')
+		exp = BeautifulSoup('')
+
+		clean_copy(inp, out, out)
+
+		self.assertEqual(out, exp, msg="On empty input, the output is changed.")
+
+	def test_basic_html_without_abip(self):
+		inp = BeautifulSoup('<html><head></head><body></body></html>')
+		out = BeautifulSoup('')
+		exp = BeautifulSoup('')
+
+		clean_copy(inp, out, out)
+
+		self.assertEqual(out, exp, msg="Unsupported tags are also added in output.")
+
+	def test_html_without_abip_2(self):
+		inp = BeautifulSoup(
+		"""
+		<html>
+		  <head>
+		  </head>
+		  <body>
+		    <div>
+		      <h1>Sample Heading</h1>
+		      <h2>Sample Heading</h2>
+		      <img src="sample.jpg" alt="example.com" width="100" height="100">
+		    </div>
+		  </body>
+		</html>
+		"""
+		)
+		out = BeautifulSoup("")
+		exp = BeautifulSoup("")
+
+		clean_copy(inp, out, out)
+
+		self.assertEqual(out, exp, msg="Unsupported tags are also added in output.")
+
 
 if __name__ == '__main__':
 	unittest.main()
