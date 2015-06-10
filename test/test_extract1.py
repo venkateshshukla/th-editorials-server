114fe62a37d19bc9950bf71888738cb36e8c16af
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2015-06-10 23:50:05 +0530

    Testing with html containing abip tags
    Also, so as to get a uniform representation of the soup object, use unicode
    method while equality check.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

--- test/test_extract1.py
+++ test/test_extract1.py
@@ -75,8 +75,59 @@ class CleanCopyTest(unittest.TestCase):
 
 		clean_copy(inp, out, out)
 
-		self.assertEqual(out, exp, msg="Unsupported tags are also added in output.")
+		self.assertEqual(unicode(out), unicode(exp))
 
+	def test_html_with_abip(self):
+		inp = BeautifulSoup(
+			"""
+			<html>
+			  <head>
+			    <title>Sample Title</title>
+			  </head>
+			  <body>
+			    <div class="class_a">
+			      <p>This is paragraph 1 of class_a.</p>
+			      <a href="http://example.com">This is a link of class_a</a>
+			    </div>
+			    <div class="class_b">
+			      <p>This is paragraph 1 of class_b.</p>
+			      <a href="http://example1.com">This is a link of class_b</a>
+			      <p>This is <b>paragraph 2</b> of <b>class_a</b>.</p>
+			      <a href="http://example2.com">This is a link of <b>class_b</b></a>
+			    </div>
+			    <div class="class_c">
+			      <p>This is paragraph 1 of class_c.</p>
+			      <a href="http://example1.com">This is a link of class_c</a>
+			      <img alt='sample1' src='sample1.png'>
+			      <p>This is <b>paragraph 2</b> of <b>class_c</b>.</p>
+			      <a href="http://example2.com">This is a link of <b>class_c<b></a>
+			      <img alt='sample2' src='sample2.png'>
+			      <p>This is <i>paragraph 3</i> of <i>class_c</i>.</p>
+			      <a href="http://example3.com">This is a link of <i>class_c</i></a>
+			      <img alt='sample3' src='sample3.png'>
+			    </div>
+			  </body>
+			</html>
+			""")
+		out = BeautifulSoup("")
+		exp = BeautifulSoup(
+			"""
+			<p>This is paragraph 1 of class_a.</p>
+			<p>This is paragraph 1 of class_b.</p>
+			<a>This is a link of class_b</a>
+			<p>This is <b>paragraph 2</b> of <b>class_a</b>.</p>
+			<a>This is a link of <b>class_b</b></a>
+			<p>This is paragraph 1 of class_c.</p>
+			<a>This is a link of class_c</a>
+			<p>This is <b>paragraph 2</b> of <b>class_c</b>.</p>
+			<a>This is a link of <b>class_c<b></a>
+			<p>This is <i>paragraph 3</i> of <i>class_c</i>.</p>
+			<a>This is a link of <i>class_c</i></a>
+			""")
+
+		clean_copy(inp, out, out)
+
+		self.assertEqual(unicode(out), unicode(exp))
 
 if __name__ == '__main__':
 	unittest.main()
